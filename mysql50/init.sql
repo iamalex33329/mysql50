7f1c40470cd78848e21fc8002d18f0902f064873
iamalex33329 <103631660+iamalex33329@users.noreply.github.com>
2022-10-01 14:49:05 +0800

    簡 -> 繁

--- mysql50/init.sql
+++ mysql50/init.sql
@@ -1,16 +1,16 @@
 USE mysql_50;
 
 -- 建 表
--- 学 生 表
+-- 學 生 表
 CREATE TABLE `Student`(
-	`s_id` VARCHAR(20),
-	`s_name` VARCHAR(20) NOT NULL DEFAULT '',
-	`s_birth` VARCHAR(20) NOT NULL DEFAULT '',
-	`s_sex` VARCHAR(10) NOT NULL DEFAULT '',
-	PRIMARY KEY(`s_id`)
+`s_id` VARCHAR(20),
+`s_name` VARCHAR(20) NOT NULL DEFAULT '',
+`s_birth` VARCHAR(20) NOT NULL DEFAULT '',
+`s_sex` VARCHAR(10) NOT NULL DEFAULT '',
+PRIMARY KEY(`s_id`)
 );
 
--- 课程表
+-- 課程表
 CREATE TABLE `Course`(
 `c_id` VARCHAR(20),
 `c_name` VARCHAR(20) NOT NULL DEFAULT '',
@@ -18,14 +18,14 @@ CREATE TABLE `Course`(
 PRIMARY KEY(`c_id`)
 );
 
--- 教师表
+-- 教師表
 CREATE TABLE `Teacher`(
 `t_id` VARCHAR(20),
 `t_name` VARCHAR(20) NOT NULL DEFAULT '',
 PRIMARY KEY(`t_id`)
 );
 
--- 成绩表
+-- 成績表
 CREATE TABLE `Score`(
 `s_id` VARCHAR(20),
 `c_id` VARCHAR(20),
@@ -33,24 +33,24 @@ CREATE TABLE `Score`(
 PRIMARY KEY(`s_id`,`c_id`)
 );
 
-insert into Student values('01' , '赵雷' , '1990-01-01' , '男');
-insert into Student values('02' , '钱电' , '1990-12-21' , '男');
-insert into Student values('03' , '孙风' , '1990-05-20' , '男');
-insert into Student values('04' , '李云' , '1990-08-06' , '男');
+insert into Student values('01' , '趙雷' , '1990-01-01' , '男');
+insert into Student values('02' , '錢電' , '1990-12-21' , '男');
+insert into Student values('03' , '孫風' , '1990-05-20' , '男');
+insert into Student values('04' , '李雲' , '1990-08-06' , '男');
 insert into Student values('05' , '周梅' , '1991-12-01' , '女');
-insert into Student values('06' , '吴兰' , '1992-03-01' , '女');
-insert into Student values('07' , '郑竹' , '1989-07-01' , '女');
-insert into Student values('09' , '张三' , '2017-12-20' , '女');
+insert into Student values('06' , '吳蘭' , '1992-03-01' , '女');
+insert into Student values('07' , '鄭竹' , '1989-07-01' , '女');
+insert into Student values('09' , '張三' , '2017-12-20' , '女');
 insert into Student values('10' , '李四' , '2017-12-25' , '女');
 insert into Student values('11' , '李四' , '2017-12-30' , '女');
-insert into Student values('12' , '赵六' , '2017-01-01' , '女');
-insert into Student values('13' , '孙七' , '2018-01-01' , '女');
+insert into Student values('12' , '趙六' , '2017-01-01' , '女');
+insert into Student values('13' , '孫七' , '2018-01-01' , '女');
 
-insert into Course values('01' , '语文' , '02');
-insert into Course values('02' , '数学' , '01');
-insert into Course values('03' , '英语' , '03');
+insert into Course values('01' , '語文' , '02');
+insert into Course values('02' , '數學' , '01');
+insert into Course values('03' , '英語' , '03');
 
-insert into Teacher values('01' , '张三');
+insert into Teacher values('01' , '張三');
 insert into Teacher values('02' , '李四');
 insert into Teacher values('03' , '王五');
 
